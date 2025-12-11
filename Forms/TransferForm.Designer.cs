bb00194f59b0709224b2c5c6d3445d311f604136
SamWang8891 <g348.8891@smashit.tw>
2025-12-11 23:57:07 +0800

    feat: TransferForm window title

--- Forms/TransferForm.Designer.cs
+++ Forms/TransferForm.Designer.cs
@@ -42,9 +42,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(263, 38);
+            label1.Location = new Point(376, 63);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(116, 54);
+            label1.Size = new Size(168, 81);
             label1.TabIndex = 0;
             label1.Text = "餘額:";
             // 
@@ -52,53 +53,59 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(179, 115);
+            label2.Location = new Point(256, 192);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(200, 54);
+            label2.Size = new Size(288, 81);
             label2.TabIndex = 1;
             label2.Text = "轉帳帳號:";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(403, 124);
+            textBox1.Location = new Point(576, 207);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(281, 43);
+            textBox1.Size = new Size(400, 61);
             textBox1.TabIndex = 2;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(403, 37);
+            label3.Location = new Point(576, 62);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(0, 54);
+            label3.Size = new Size(0, 81);
             label3.TabIndex = 3;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(179, 187);
+            label4.Location = new Point(256, 312);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(200, 54);
+            label4.Size = new Size(288, 81);
             label4.TabIndex = 4;
             label4.Text = "轉帳金額:";
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(403, 197);
+            textBox2.Location = new Point(576, 328);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(281, 43);
+            textBox2.Size = new Size(400, 61);
             textBox2.TabIndex = 5;
             // 
             // button1
             // 
             button1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(221, 297);
+            button1.Location = new Point(316, 495);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(175, 42);
+            button1.Size = new Size(250, 70);
             button1.TabIndex = 6;
             button1.Text = "返回";
             button1.UseVisualStyleBackColor = true;
@@ -107,9 +114,10 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(444, 297);
+            button2.Location = new Point(634, 495);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(175, 42);
+            button2.Size = new Size(250, 70);
             button2.TabIndex = 7;
             button2.Text = "轉帳";
             button2.UseVisualStyleBackColor = true;
@@ -117,9 +125,9 @@
             // 
             // TransferForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(825, 386);
+            ClientSize = new Size(1179, 643);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox2);
@@ -128,9 +136,10 @@
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "TransferForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
+            Text = "轉帳";
             ResumeLayout(false);
             PerformLayout();
         }
